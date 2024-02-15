6fbd09a74ff10487aea6e54352617a4f612718e8
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-15 16:24:33 +0000

    Create start_game function

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def start_game():
+    print("Welcome to It All Goes Down game. \n")
+    user_answer = input("Would you like to embark on this adventure? (Yes/No) \n")
+    if user_answer == "Yes":
+        print("Great, let's get started.")
+    elif user_answer == "No":
+        print("Oh no, it's so sad to see you go.")
+    else:
+        print("You've entered an invalid value, please reply with a Yes or No.")
+
+start_game()
