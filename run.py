61563341a3c82a04ee8331e31e47a8e2b69b9731
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-15 16:49:47 +0000

    Add while loop to run program until a valid value is given

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 def start_game():
-    print("Welcome to It All Goes Down game. \n")
-    user_answer = input("Would you like to embark on this adventure? (Yes/No) \n")
-    if user_answer == "Yes":
-        print("Great, let's get started.")
-    elif user_answer == "No":
-        print("Oh no, it's so sad to see you go.")
-    else:
-        print("You've entered an invalid value, please reply with a Yes or No.")
+    while True:
+        print("Welcome to It All Goes Down game. \n")
+        user_answer = input("Would you like to embark on this adventure? (Yes/No)\n")
+        if user_answer.capitalize() == "Yes":
+            print("Great, let's get started. \n")
+            break
+        elif user_answer.capitalize() == "No":
+            print("Oh no, it's so sad to see you go. \n")
+            break
+        else:
+            print("You've entered an invalid value, please reply with a Yes or No. \n")
 
 start_game()
