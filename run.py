8ccbeec643caacc7a277a0ee65adbe6fe842891c
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-16 09:10:11 +0000

    Create an intro question function that asks user if they want to play

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-def start_game():
+def intro_question():
+    """
+    Welcomes player to the game and asks question to begin the game
+    """
     while True:
         print("Welcome to It All Goes Down game. \n")
         user_answer = input("Would you like to embark on this adventure? (Yes/No)\n")
         if user_answer.capitalize() == "Yes":
             print("Great, let's get started. \n")
+            start_game()
             break
         elif user_answer.capitalize() == "No":
             print("Oh no, it's so sad to see you go. \n")
@@ -11,4 +15,15 @@ def start_game():
         else:
             print("You've entered an invalid value, please reply with a Yes or No. \n")
 
-start_game()
+
+def start_game():
+    """
+    Start game function, asks the player to provide a character name,
+    and begins to unfold the story of this game.
+    """
+    player_name = input("What is your characters name? \n")
+    print(f"Nice to meet you {player_name.capitalize()}. Let the adventure begin... \n")
+    print("After years and years of hard work, blood, sweat and tears, you have finally") 
+    print("managed to save enough money to embark on that journey you've been longing since you were a child.")
+
+intro_question()
