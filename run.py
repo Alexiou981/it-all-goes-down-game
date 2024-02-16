9c54ffccf3bdffcaea28bbf9db794fab027ee486
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-16 16:47:57 +0000

    chore: add newline of code for each sentence

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ def intro_question():
     """
     slowprint("Welcome to It All Goes Down game. \n")
     while True:
-        slowprint("Would you like to embark on this adventure? (Yes/No)")
+        slowprint("Would you like to embark on this adventure? (Yes/No) \n")
         user_answer = input()
         if user_answer.capitalize() == "Yes":
-            slowprint("Great, let's get started. \n")
+            slowprint("Great, let's get started.\n")
             start_game()
             break
         elif user_answer.capitalize() == "No":
@@ -31,12 +31,12 @@ def start_game():
     else:
         slowprint(f"Nice to meet you {player_name.capitalize()}. Let the adventure begin... \n")
     
-    slowprint("After years and years of hard work, blood, sweat and tears...") 
-    slowprint("You have finally managed to save enough money to embark on")
+    slowprint("After years and years of hard work, blood, sweat and tears...\n") 
+    slowprint("You have finally managed to save enough money to embark on\n")
     slowprint("that journey you've been longing for since you were a child. \n")
-    slowprint("The destination is a magical island in the middle of the Pacific...") 
+    slowprint("The destination is a magical island in the middle of the Pacific...\n") 
     slowprint("Crystal clear waters and golden sands covering it's perimeter. \n") 
-    slowprint("Your departure destination is far away from this location and") 
+    slowprint("Your departure destination is far away from this location and\n") 
     slowprint("your budget allows you to either fly or sail there... \n")
 
 def choose_transportation():
@@ -44,17 +44,17 @@ def choose_transportation():
     Means of transport functions, asks the user which vehicle of choice
     they prefer and unfolds storing depending on players choice.
     """
-    slowprint("How would you like to travel? (Plane/Ship)")
+    slowprint("How would you like to travel? (Plane/Ship)\n")
     transportation_vehicle = input()
     if transportation_vehicle == "Plane":
-        slowprint("You have chosen to travel by aeroplane...")
-        slowprint("You arrive at the airport and check in...")
-        slowprint("You board the aircraft and your flight takes off...")
+        slowprint("You have chosen to travel by aeroplane...\n")
+        slowprint("You arrive at the airport and check in...\n")
+        slowprint("You board the aircraft and your flight takes off...\n")
     elif transportation_vehicle == "Ship":
-        slowprint("You have chosen to travel by ship...")
-        slowprint("You arrive at the port, get your tickets and board the ship...")
-        slowprint("As soon as you locate the cabin the ship sets sale...")
-        slowprint("Your voyage begins...")
+        slowprint("You have chosen to travel by ship...\n")
+        slowprint("You arrive at the port, get your tickets and board the ship...\n")
+        slowprint("As soon as you locate the cabin the ship sets sale...\n")
+        slowprint("Your voyage begins...\n")
     else:
         print("You have entered an incorrect value!!!")
         print("Means of transportation needs to be either Plane or Ship")
