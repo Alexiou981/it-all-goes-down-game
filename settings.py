3e38129b83d28edd985057f21c64f61950e55b33
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-16 15:41:40 +0000

    fix: change slowprint function for faster printing

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 import sys
 import time
-# This function taken from this source: https://www.grepper.com/answers/51377/python+slow+print
-def slowprint(text):
-    for letter in text + "\n":
+# This function taken from this source: 
+# https://github.com/DebbieBergstrom/Russian-Roulette/blob/main/all_functions.py
+# By Debbie Bergstrom, def slow_print() function.
+def slowprint(text, delay=0.03):
+    for letter in text:
         sys.stdout.write(letter)
         sys.stdout.flush()
-        time.sleep(1./10)
+        time.sleep(delay)
         
