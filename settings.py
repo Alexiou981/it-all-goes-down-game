f0eab7c9a36955368f02f63a65392c12e4b731bf
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-16 10:51:31 +0000

    Add code for slowprint function

---
+++ settings.py
@@ -0,0 +1,11 @@
+import sys
+import time
+# This function taken from this source: https://www.grepper.com/answers/51377/python+slow+print
+def slowprint(text):
+    for letter in text + "\n":
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.8/10)
+
+slowprint("this is written slowly on the terminal")
+        
