7663b93e2701b91cb24f3e09345c5a06ceb22da0
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-17 12:36:17 +0000

    chore: refactor slowprint statements and use concatenation to join sentences together

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ def intro_question():
     """
     Welcomes player to the game and asks question to begin the game
     """
-    slowprint("Welcome to It All Goes Down game. \n")
+    slowprint("Welcome to It All Goes Down game.\n\n")
     while True:
-        slowprint("Would you like to embark on this adventure? (Yes/No) \n")
-        user_answer = input()
+        slowprint("Would you like to embark on this adventure? (Yes/No)\n")
+        user_answer = input("")
         if user_answer.capitalize() == "Yes":
-            slowprint("Great, let's get started.\n")
+            slowprint("\nGreat, let's get started.\n")
             start_game()
             break
         elif user_answer.capitalize() == "No":
@@ -24,23 +24,24 @@ def start_game():
     Start game function, asks the player to provide a character name,
     and begins to unfold the story of the game.
     """
-    player_name = input("What is your characters name? \n")
+    player_name = input("\nWhat is your characters name?\n")
     while not player_name.isalpha():
         print("You have entered an invalid value!!!")
-        player_name = input("What is your characters name? \n")
+        player_name = input("What is your characters name?\n\n")
         continue
     else:
-        slowprint(f"Nice to meet you {player_name.capitalize()}. Let the adventure begin... \n")
+        slowprint(f"\nNice to meet you {player_name.capitalize()}. Let the adventure begin... \n")
         
         
     
-    slowprint("After years and years of hard work, blood, sweat and tears...\n") 
-    slowprint("You have finally managed to save enough money to embark on\n")
-    slowprint("that journey you've been longing for since you were a child. \n")
-    slowprint("The destination is a magical island in the middle of the Pacific...\n") 
-    slowprint("Crystal clear waters and golden sands covering it's perimeter. \n") 
-    slowprint("Your departure destination is far away from this location and\n") 
-    slowprint("your budget allows you to either fly or sail there... \n")
+    slowprint(
+        "\nAfter years and years of hard work, blood, sweat and tears...\n"+ 
+        "You have finally managed to save enough money to embark on\n"+
+        "that journey you've been longing for since you were a child.\n\n"+
+        "The destination is a magical island in the middle of the Pacific...\n"+
+        "crystal clear waters and golden sands covering it's perimeter.\n\n"+
+        "Your departure destination is far away from this location and\n"+
+        "your budget allows you to either fly or sail there...\n\n")
 
 def choose_transportation():
     """
@@ -50,14 +51,16 @@ def choose_transportation():
     slowprint("How would you like to travel? (Plane/Ship)\n")
     transportation_vehicle = input()
     if transportation_vehicle.capitalize() == "Plane":
-        slowprint("You have chosen to travel by aeroplane...\n")
-        slowprint("You arrive at the airport and check in...\n")
-        slowprint("You board the aircraft and your flight takes off...\n")
+        slowprint(
+            "You have chosen to travel by aeroplane...\n" +
+            "You arrive at the airport and check in...\n" +
+            "You board the aircraft and your flight takes off...\n")
     elif transportation_vehicle.capitalize() == "Ship":
-        slowprint("You have chosen to travel by ship...\n")
-        slowprint("You arrive at the port, get your tickets and board the ship...\n")
-        slowprint("As soon as you locate the cabin the ship sets sale...\n")
-        slowprint("Your voyage begins...\n")
+        slowprint(
+            "You have chosen to travel by ship...\n" +
+            "You arrive at the port, get your tickets and board the ship...\n" +
+            "As soon as you locate the cabin the ship sets sale...\n" +
+            "Your voyage begins...\n")
     else:
         print("You have entered an incorrect value!!!")
         print("Means of transportation needs to be either Plane or Ship")
