39eef4151d3cc32562cf14861911a158bba03bec
EVANGELOS ALEXIOU <alexsander@MacBook-Pro.local>
2024-02-20 14:05:05 +0000

    chore: adjust code to pass PEP8 linter

--- settings.py
+++ settings.py
@@ -1,14 +1,17 @@
 import sys
 import time
-# This function taken from this source: 
+# This function taken from this source:
 # https://github.com/DebbieBergstrom/Russian-Roulette/blob/main/all_functions.py
 # By Debbie Bergstrom, def slow_print() function.
+
+
 def slowprint(text, delay=0.01):
     for letter in text:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(delay)
-        
+
+
 def slowprint_ascii(text, delay=0.01):
     for letter in text:
         sys.stdout.write(letter)
